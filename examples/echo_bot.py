bb9bac870d133a2518bc74a278bf4213d1abc98d
Norbox4 <77930352+Norbox4@users.noreply.github.com>
2025-02-03 21:43:12 +0100

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -5,15 +5,15 @@
 
 import telebot
 
-API_TOKEN = '<api_token>'
+API_TOKEN = '<7255443982:AAG_XZc4SLXqD9x8cTf8l3GT5ybcje0QnwY>'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
+def send_welcome(ciao):
+    bot.reply_to(naia, """\
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
@@ -22,7 +22,12 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+if message.text=='Ciao':
+        bot.reply_to(message, 'Ciao io sono un gay, come stai?')
+elif message.text=='Bene':
+    bot.reply_to(message, 'Bene anchio sono gay')
+else: 
+ bot.reply_to(message, 'Sono gay, non capisco')
 
 
 bot.infinity_polling()
